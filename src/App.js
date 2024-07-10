19e1608caf21cc1e7e288cb995ea75b61276168d
idosh <Idosh360@gmail.com>
2024-07-11 01:29:14 +0300

    two buttons and future dropdown, and they squash

--- src/App.js
+++ src/App.js
@@ -8,12 +8,33 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            React_Test_1
-          </p>
-          <div className="rounded-rectangle" onClick={() => window.location.href="https://tinyurl.com/idosh3d"}>
-              I can make websites now yay
-          </div>
+          <p>React_Test_1</p>
+          <jojo
+            className="rounded-rectangle"
+            onClick={() => window.location.href="https://tinyurl.com/idosh3d"}
+            >
+            I can make websites now yay
+          </jojo>
+          <jojo2
+            className="rounded-rectangle"
+            onClick={() => window.open("https://tinyurl.com/idosh3d", "_blank")}
+            style={{ marginTop: '20px' }}
+            >
+            I can make websites now yay
+          </jojo2>
+
+          <dropdown
+            className="rounded-rectangle"
+            style={{
+              backgroundColor: 'rgb(100, 180, 180)',
+              width: '300px',
+              height: '50px',
+              position: 'absolute',
+              top: '80px',
+              right: '80px',
+            }}
+            >
+          </dropdown>
         </header>
       </div>
     );
